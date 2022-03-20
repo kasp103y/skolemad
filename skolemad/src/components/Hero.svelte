226f945b310f1e2d1fc4baddc4ea97e5352ff1ad
Kasper Nikolajsen Dahl <kasp103y@stud.kea.dk>
2022-03-20 12:41:23 +0100

    hero-lysere

--- skolemad/src/components/Hero.svelte
+++ skolemad/src/components/Hero.svelte
@@ -3,16 +3,18 @@
 </script>
 
 <section class="hero">
-  <h1>SkoleMad</h1>
-  <p class="p-lead">
-    SkoleMad tilbyder en nem og fleksibel frokostordning til dine børn
-  </p>
-  <Button>Log ind/Opret bruger</Button>
+  <div>
+    <h1>SkoleMad</h1>
+    <p class="p-lead">
+      SkoleMad tilbyder en nem og fleksibel frokostordning til dine børn
+    </p>
+    <Button>Log ind/Opret bruger</Button>
+  </div>
 </section>
 
 <style>
   .hero {
-    background-image: url("../barn-mad.jpg");
+    background-image: url("../barn-mad-1.jpg");
     background-repeat: no-repeat;
     background-size: cover;
     min-height: 90vh;
@@ -20,5 +22,13 @@
     display: grid;
     justify-content: center;
     align-content: center;
+  }
+
+  .hero > * {
+    margin-top: var(--spacing-2);
+  }
+
+  div {
+    max-width: 20vw;
   }
 </style>
