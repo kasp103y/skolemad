8c36af9b7045802ae33729aeedffff9490b64baf
jaco7565 <jaco7565@stud.kea.dk>
2022-03-20 12:08:46 +0100

    jj

--- skolemad/src/App.svelte
+++ skolemad/src/App.svelte
@@ -2,7 +2,22 @@
   import "carbon-components-svelte/css/all.css";
   import { Accordion, AccordionItem } from "carbon-components-svelte";
   import Hero from "./components/Hero.svelte";
+  import Bestil from "./Bestil.svelte";
+  import NavBar from "./NavBar.svelte";
+  import Konto from "./Konto.svelte";
+  let page = "front";
 </script>
+
+<NavBar>
+  <a href="#/bestil" on:click={() => (page = "bestil")}>Bestil </a>
+  <a href="#/konto" on:click={() => (page = "Konto")}>Konto </a>
+</NavBar>
+<h1>Hello {page}!</h1>
+{#if page === "bestil"}
+  <Bestil />
+{:else if page === "konto"}
+  <Konto />
+{/if}
 
 <main>
   <Hero />
