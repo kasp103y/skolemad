e55bc53811e7d361122ca4cdbea915f098a94796
Kasper Nikolajsen Dahl <kasp103y@stud.kea.dk>
2022-03-20 12:10:22 +0100

    hero

--- skolemad/src/App.svelte
+++ skolemad/src/App.svelte
@@ -2,6 +2,7 @@
   import "carbon-components-svelte/css/all.css";
   import { Accordion, AccordionItem } from "carbon-components-svelte";
   import Hero from "./components/Hero.svelte";
+  import Button from "./components/Button.svelte";
 </script>
 
 <main>
